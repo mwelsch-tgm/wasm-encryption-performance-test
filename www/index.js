7c68d1bb77fe0de573144a38bf9bf5553ece46d9
Moritz Welsch <mwelsch@student.tgm.ac.at>
2019-10-27 00:00:15 +0200

    Beautified table and added avg, min and max time

--- www/index.js
+++ www/index.js
@@ -8,8 +8,8 @@ const runTests = () => {
     var times = document.getElementById("number1").value;
     var times2 = document.getElementById("number2").value;
     var cryptos = ["c2chacha20_encrypt_decrypt", "blowfish_cbc_encrypt_decrypt", "rust_crypto_aes_encrypt_decrypt", "aes_256_gcm_siv_encrypt_decrypt", "rust_crypto_blowfish_encrypt_decrypt"];
-    var table = "<table> <tr><th>Methode</th><th>time (ms)</th></tr>";
     var runTimes = new Map();
+    //TODO add pairs to map dynamically, depending on the cryptos array OR replace the usage of cryptos array with the map
     runTimes.set("c2chacha20_encrypt_decrypt", []);
     runTimes.set("blowfish_cbc_encrypt_decrypt", []);
     runTimes.set("rust_crypto_aes_encrypt_decrypt", []);
@@ -49,10 +49,25 @@ const runTests = () => {
             var afterTest = new Date().getTime();
             var diff = afterTest-beforeTest;
             var currArray = runTimes.get(cryptos[i]);
-            runTimes.set(cryptos[i],currArray.push(diff));
+            currArray.push(diff)
+            runTimes.set(cryptos[i],currArray);
         }
     }
 
+    //generate table
+    var table = "<table> <tr><th>Methode</th><th>avg time [ms]</th><th>max time [ms]</th><th>min time [ms]</th></tr>";
+    for (let i = 0; i<cryptos.length;i++){
+        var sum = 0;
+        var array = runTimes.get(cryptos[i]);
+        for( var j = 0; j < array.length; j++ ){
+            sum += parseInt( array[i], 10 );
+        }
+        var avg = sum/array.length;
+        var min = Math.min.apply(Math,array);
+        var max = Math.max.apply(Math,array);
+        table += "<tr><td>" + cryptos[i] + "</td><td> "+ avg +"</td><td> "+ max +"</td><td> "+ min +"</td></tr>";
+
+    }
     table += "</table>";
 
     document.getElementById("tbl").innerHTML += table;
